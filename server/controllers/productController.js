4da3eb337f55d70976831e984d47f4f2e3357a32
Oktay Dönmez <oktaydonmez98@gmail.com>
2020-12-13 14:36:38 +0300

    fix bugs while adding products other than n11

--- server/controllers/productController.js
+++ server/controllers/productController.js
@@ -1,10 +1,12 @@
 const asyncWrapper = require('../helpers/asyncWrapper');
 const Product = require('../models/Product');
 const Price = require('../models/Price');
+const cheerio = require('cheerio');
+const axios = require('axios');
 const CustomError = require('../helpers/CustomError');
 const inputHelper = require('../helpers/inputHelpers');
-const getProductId = require('../helpers/getProductId');
-const getProductPrice = require('../helpers/getProductPrice');
+const n11ProductId = require('../helpers/n11ProductId');
+const n11ProductPrice = require('../helpers/n11ProductPrice');
 
 const products = asyncWrapper(async (req, res, next) => {
   const data = await Product.aggregate([
@@ -52,13 +54,32 @@ const newProduct = asyncWrapper(async (req, res, next) => {
     return next(new CustomError('Check your inputs. All fields are required.', 400));
   }
 
-  const productId = await getProductId(req, res, next);
-
   const info = req.body;
-  if (info.url.includes('n11.com'))
-    info.url = `https://www.n11.com/component/render/newProductPriceArea?productId=${productId}`;
 
-  const productPrice = await getProductPrice(req, res, next);
+  if (info.url.includes('n11.com')) {
+    const productId = await n11ProductId(req, res, next);
+    info.url = `https://www.n11.com/component/render/newProductPriceArea?productId=${productId}`;
+    const productPrice = await n11ProductPrice(req, res, next);
+
+    const newData = await Product.create({
+      name: info.name,
+      url: info.url,
+      selector: info.selector,
+    });
+
+    const price = await Price.create({
+      product: newData._id,
+      price: productPrice,
+    });
+
+    return res.status(200).json({
+      success: true,
+      data: {
+        newData,
+        price,
+      },
+    });
+  }
 
   const newData = await Product.create({
     name: info.name,
@@ -66,9 +87,13 @@ const newProduct = asyncWrapper(async (req, res, next) => {
     selector: info.selector,
   });
 
+  const html = axios.get(newData.url);
+  const $ = cheerio.load(html.data);
+  const priceText = $(newData.selector).text().trim();
+
   const price = await Price.create({
     product: newData._id,
-    price: productPrice,
+    price: priceText,
   });
 
   res.status(200).json({
--- server/helpers/n11ProductPrice.js
+++ server/helpers/n11ProductPrice.js
@@ -2,15 +2,14 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const CustomError = require('./CustomError');
 
-const getProductId = async (req, res, next) => {
+const getProductPrice = async (req, res, next) => {
   const html = await axios.get(req.body.url);
   if (html.status !== 200) return next(new CustomError('Page not found!', 404));
 
   const $ = cheerio.load(html.data);
-  const pId = $('[data-uniproductid]').attr('data-uniproductid');
-  console.log(pId);
+  const price = $('.newPrice').text().trim();
 
-  return pId;
+  return price;
 };
 
-module.exports = getProductId;
+module.exports = getProductPrice;
